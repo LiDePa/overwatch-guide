493401f80d6354ae1e3efb9242902b528b39bc3f
Linus De Pascale <linus.depascale@hs-augsburg.de>
2020-12-08 16:43:33 +0100

    make path to all_hero_names.json relative

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import json
+import json, os
 
 
 app = Flask(__name__)
 
-#get array with all hero names which is used to generate all hero pages and buttons
-with open('static/all_hero_names.json') as (d):
-    all_hero_names = json.load(d)
+#get all_hero_names array from json file in static folder
+#is used to verify urls
+path = os.path.join(app.root_path, 'static', 'all_hero_names.json')
+with open(path) as (data):
+    all_hero_names = json.load(data)
 
 
-#index.html links to all heroes in all_hero_names
-#user decides if it links to questions or answers
+#buttons to each hero in all_hero_names are created by script inside index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#questions page to fill database
+#questions page for each hero in all_hero_names
+#is used to fill database
 @app.route('/<hero_name>-questions')
 def questions(hero_name):
-    #check if url actually contains a hero
-    if hero_name in all_hero_names:
-        return render_template('questions.html', hero_name=hero_name)
-    else: 
-        return "Hero not found."
+	#check if url actually contains a hero
+	if hero_name in all_hero_names:
+		return render_template('questions.html', hero_name=hero_name)
+	else:
+		return "Hero not found."
 
-#answers page to display contents of database
+#answers page for each hero in all_hero_names
+#displays contents of database
 @app.route('/<hero_name>-answers')
 def answers(hero_name):
-    #check if url actually contains a hero
-    if hero_name in all_hero_names:
-        return render_template('answers.html', hero_name=hero_name)
-    else: 
-        return "Hero not found."
+	#check if url actually contains a hero
+	if hero_name in all_hero_names:
+		return render_template('answers.html', hero_name=hero_name)
+	else: 
+		return "Hero not found."
 
 
 if __name__ == '__main__':
