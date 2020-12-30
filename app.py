bf13f9a4b7615a591b69ae98821b6d7bb228d8ab
Linus De Pascale <linus.depascale@hs-augsburg.de>
2020-12-30 22:50:21 +0100

    add processing of multiple checkbox selections and implement commonness funtionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 import json, os
 
 
@@ -19,7 +20,7 @@ class Question1(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	current_hero = db.Column(db.String(30))
 	selected_result = db.Column(db.String(30))
-	commonness = db.Column(db.Integer, default = 0)
+	commonness = db.Column(db.Integer, default = 1)
 #	def __repr__(self):
 #        return '<Task %r>' % self.id
 
@@ -36,12 +37,18 @@ def index():
 @app.route('/<hero_name>-questions', methods=['POST', 'GET'])
 def questions(hero_name):
 	if request.method == 'POST':
-		# current_hero = request.form['']
-		hero_result = request.form["hero_result"]
-		new_data = Question1(current_hero = hero_name, selected_result = hero_result)
-		db.session.add(new_data)
-		db.session.commit()
-		return "Submitted succesfully"
+		#get selected heroes as position in all_hero_names and turn them into string
+		for n in request.form.getlist("hero_result"):
+			hero_result = all_hero_names[int(n)]
+			#if the combination of current_hero and selected_result already exists in table, increase its commonness value
+			if db.session.query(db.exists().where(and_(Question1.current_hero == hero_name, Question1.selected_result == hero_result))).scalar() == True:
+				existing_data = Question1.query.filter(and_(Question1.current_hero == hero_name, Question1.selected_result == hero_result)).first()
+				existing_data.commonness += 1
+			else:
+				new_data = Question1(current_hero = hero_name, selected_result = hero_result)
+				db.session.add(new_data)
+			db.session.commit()
+		return "Submitted successfully"
 	else:
 		#check if url actually contains a hero
 		if hero_name in all_hero_names:
